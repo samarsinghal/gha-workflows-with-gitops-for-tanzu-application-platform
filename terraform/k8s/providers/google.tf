c74496506208536b9b763696a545c0ca7d15dc75
samar singhal <samarsinghal@gmail.com>
2023-02-24 16:56:56 -0700

    Update google.tf

--- terraform/k8s/providers/google.tf
+++ terraform/k8s/providers/google.tf
@@ -1,7 +1,15 @@
+provider "kubernetes" {
+  config_path = var.kubeconfig_path
+}
+
 provider "google" {
   project = var.project
 }
 
 variable "project" {
   description = "The target active Google project id"
+}
+
+variable "kubeconfig_path" {
+  description = "kubeconfig path"
 }
